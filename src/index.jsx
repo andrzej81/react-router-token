1b3e77f675d442a3189ab45bff594ba9a7f71e8d
Andrzej Iwaniuk <andrzej.iwaniuk@live.co.uk>
2022-02-01 00:03:54 +0000

    add some data json

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Preferences from "./components/Preferences/Preferences.jsx";
-import App from "./App";
+import Invoices from "./components/Invoices/Invoice.jsx";
 
 const rootElement = document.getElementById("root");
 render(
@@ -12,6 +13,15 @@ render(
       <Route path="/" element={<App />} />
       <Route path="dashboard" element={<Dashboard />} />
       <Route path="preferences" element={<Preferences />} />
+      <Route path="invoices" element={<Invoices />} />
+      <Route
+      path="*"
+      element={
+        <main style={{ padding: "1rem" }}>
+          <p>There's nothing here!</p>
+        </main>
+      }
+    />
     </Routes>
   </BrowserRouter>,
   rootElement
